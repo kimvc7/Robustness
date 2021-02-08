a94266f2c8ec32fd4f2cd1b497b9ee40bd22a5f4
xboix <xboix@mit.edu>
2021-02-08 14:59:48 -0500

    adjust hyperparameters

--- runs/config_experiments_synthetic.py
+++ runs/config_experiments_synthetic.py
@@ -9,7 +9,6 @@ def config_experiments(results_dir, create_json=True):
 
     id = 0
     experiment_list = []
-    # Models with robustness
     for dataset in range(2, 21):
 
         for net in ["MLP"]:
@@ -68,11 +67,52 @@ def config_experiments(results_dir, create_json=True):
                     experiment_list.append(config.copy())
                     id += 1
 
-        ''' 
+    # Secound round: smaller epsilons and combining pgd+robustness
+    for dataset in range(2, 21):
+
+        for net in ["MLP"]:
+            for lr in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]:
+                for epsilon in [1e-4, 1e-5]:
+                    config = base_config.copy()
+                    config["data_set"] = dataset
+                    config["model_name"] = str(id)
+                    config["backbone"] = net
+                    config["initial_learning_rate"] = lr
+                    config["epsilon"] = epsilon
+                    config["robust_training"] = True
+                    config["pgd_training"] = False
+                    config["batch_decrease_learning_rate"] = 1e10  # do not decrease the learning rate
+
+                    if create_json:
+                        with open(results_dir + 'configs/' + str(id) + '.json', 'w') as json_file:
+                            json.dump(config, json_file)
+                    experiment_list.append(config.copy())
+                    id += 1
+
         for net in ["MLP+pgd"]:
             for lr in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]:
-                for epsilon in [1e-3, 1e-2, 1e-1, 1]:
-                    for epsilon_pgd_training in [1e-3, 1e-2, 1e-1, 1]:
+                for epsilon_pgd_training in [1e-4, 1e-5]:
+                    config = base_config.copy()
+                    config["data_set"] = dataset
+                    config["model_name"] = str(id)
+                    config["backbone"] = net
+                    config["initial_learning_rate"] = lr
+                    config["epsilon"] = epsilon
+                    config["robust_training"] = False
+                    config["pgd_training"] = True
+                    config["epsilon_pgd_training"] = epsilon_pgd_training
+                    config["batch_decrease_learning_rate"] = 1e10  # do not decrease the learning rate
+
+                    if create_json:
+                        with open(results_dir + 'configs/' + str(id) + '.json', 'w') as json_file:
+                            json.dump(config, json_file)
+                    experiment_list.append(config.copy())
+                    id += 1
+
+        for net in ["MLP+pgd"]:
+            for lr in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]:
+                for epsilon in [1e-2, 1e-3, 1e-4, 1e-5]:
+                    for epsilon_pgd_training in [1e-2, 1e-3, 1e-4, 1e-5]:
                         config = base_config.copy()
                         config["data_set"] = dataset
                         config["model_name"] = str(id)
@@ -89,7 +129,7 @@ def config_experiments(results_dir, create_json=True):
                                 json.dump(config, json_file)
                         experiment_list.append(config.copy())
                         id += 1
-        '''
+
 
     print(str(id) + " config files created")
     return experiment_list
