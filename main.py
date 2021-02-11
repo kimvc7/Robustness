a0012058c59d5d4237c5c4f4670e4b25dbd74958
Xavier Biox Bosch <xboix@dgx1.mit.edu>
2021-02-11 15:15:09 -0500

    dgx

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if args.filesystem == 'local':
 elif args.filesystem == 'om':
     results_dir = '/vast/robustness/'
 elif args.filesystem == 'dgx1':
-    results_dir = '/raid/poggio/home/xboix/results/Robustness'
+    results_dir = '/raid/poggio/home/xboix/results/Robustness/'
 
 full_results_dir = results_dir + args.experiment_name + '/'
 
