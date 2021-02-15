4ebe5e3718245d82f43553e3cdb8f18045cdec2c
xboix <xboix@mit.edu>
2021-02-14 22:31:06 -0500

    UCI cross-val

--- slurm/test.sh
+++ slurm/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --array=0-989
+#SBATCH --array=0-94
 #SBATCH -n 1
 #SBATCH -c 2
 #SBATCH --job-name=robustness
-#SBATCH --exclude=node028,node022,node030,node025,node023,node004,node003,node021,node022,node026,node089
+#SBATCH --exclude=node071,node072,node073,node074,node075,node076,node028,node022,node030,node025,node023,node004,node003,node021,node026,node022,node089
 #SBATCH --mem=4GB
-#SBATCH -t 0:10:00
+#SBATCH -t 0:5:00
 #SBATCH -D ./log/
 #SBATCH --partition=use-everything
 #SBATCH --gres=gpu:1
@@ -19,9 +19,9 @@ echo $CUDA_DEVICE_ORDER
 
 singularity exec -B /om:/om -B /om2:/om2 -B /scratch/user/xboix:/vast --nv /om/user/xboix/singularity/xboix-tensorflow2.4.simg \
 python3 main.py \
---experiment_id=$((2000+${SLURM_ARRAY_TASK_ID})) \
+--experiment_id=$((0+${SLURM_ARRAY_TASK_ID})) \
 --filesystem=om \
---experiment_name=uci \
+--experiment_name=mnist_std \
 --run=test \
 --gpu_id=0
 
